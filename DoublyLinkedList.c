2ceed6e973577bc3239834f9e61169dc690994d9
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-01-07 22:33:42 +0530

    made it prettier

--- DoublyLinkedList.c
+++ DoublyLinkedList.c
@@ -1,58 +1,67 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct NODE{
+typedef struct NODE
+{
     struct NODE *left;
     struct NODE *right;
     int data;
-}*node;
+} *node;
 
-typedef struct{
+typedef struct
+{
     node head;
-}list;
+} list;
 
-node getnode(int data){
-    node n=(struct NODE *)malloc(sizeof(struct NODE));
-    n->data=data;
+node getnode(int data)
+{
+    node n = (struct NODE *)malloc(sizeof(struct NODE));
+    n->data = data;
     return n;
 }
 
-void init(list *l){
-    l->head->left=NULL;
-    l->head->right=NULL;
+void init(list *l)
+{
+    l->head->left = NULL;
+    l->head->right = NULL;
 }
 
-void insert_front(list *l,int key){
-    node n=getnode(key);
-    if(l->head=NULL){
-        l->head=n;
-        n->left=NULL;
-        n->right=NULL;
+void insert_front(list *l, int key)
+{
+    node n = getnode(key);
+    if (l->head == NULL)
+    {
+        l->head = n;
+        n->left = NULL;
+        n->right = NULL;
     }
-    n->right=l->head;
-    n->left=NULL;
-    l->head->left=n;
-    l->head=n;
+    n->right = l->head;
+    n->left = NULL;
+    l->head->left = n;
+    l->head = n;
 }
 
-void display(list *l){
-    node cur=l->head;
-    if(cur==NULL)
+void display(list *l)
+{
+    node cur = l->head;
+    if (cur == NULL)
         return;
-    while(cur!=NULL){
-        printf("%d->",cur->data);
-        cur=cur->right;
+    while (cur != NULL)
+    {
+        printf("%d->", cur->data);
+        cur = cur->right;
     }
 }
 
-int main(){
+int main()
+{
     list l;
     init(&l);
-    insert_front(&l,10);
-    insert_front(&l,20);
-    insert_front(&l,30);
-    insert_front(&l,40);
-    insert_front(&l,50);
-    insert_front(&l,60);
+    insert_front(&l, 10);
+    insert_front(&l, 20);
+    insert_front(&l, 30);
+    insert_front(&l, 40);
+    insert_front(&l, 50);
+    insert_front(&l, 60);
     display(&l);
 }
--- SingleLinkedLIst.c
+++ SingleLinkedLIst.c
@@ -1,59 +1,69 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct NODE{
+typedef struct NODE
+{
     int data;
     struct NODE *next;
-}*node;
+} *node;
 
-typedef struct{
+typedef struct
+{
     node head;
-}list;
+} list;
 
-void getnode(node *n){
-    *n=(struct NODE *)malloc(sizeof(struct NODE));
+void getnode(node *n)
+{
+    *n = (struct NODE *)malloc(sizeof(struct NODE));
 }
 
-void insert_front(list *l,int data){
+void insert_front(list *l, int data)
+{
     node cur;
     getnode(&cur);
-    cur->data=data;
-    cur->next=l->head;
-    l->head=cur;
+    cur->data = data;
+    cur->next = l->head;
+    l->head = cur;
 }
 
-void display(list *l){
-    node cur=l->head;
-    while(cur!=NULL){
-        printf("%d->",cur->data);
-        cur=cur->next;
+void display(list *l)
+{
+    node cur = l->head;
+    while (cur != NULL)
+    {
+        printf("%d->", cur->data);
+        cur = cur->next;
     }
     printf("\n");
 }
 
-void delete_rear(list *l){
-    node cur=l->head;
-    if(cur->next==NULL){
+void delete_rear(list *l)
+{
+    node cur = l->head;
+    if (cur->next == NULL)
+    {
         free(cur);
-        l->head=NULL;
+        l->head = NULL;
         return;
     }
-    while(cur->next->next!=NULL){
-        cur=cur->next;
+    while (cur->next->next != NULL)
+    {
+        cur = cur->next;
     }
-    node temp=cur;
-    cur->next=NULL;
+    node temp = cur;
+    cur->next = NULL;
     free(temp->next);
 }
 
-int main(){
+int main()
+{
     list l;
-    l.head=NULL;
-    insert_front(&l,10);
-    insert_front(&l,20);
-    insert_front(&l,30);
-    insert_front(&l,40);
-    insert_front(&l,50);
+    l.head = NULL;
+    insert_front(&l, 10);
+    insert_front(&l, 20);
+    insert_front(&l, 30);
+    insert_front(&l, 40);
+    insert_front(&l, 50);
     display(&l);
     delete_rear(&l);
     display(&l);
